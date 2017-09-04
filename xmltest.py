c544c3f222772859a637d0258fb0dea456cd745e
yanlongliang <573477106@qq.com>
2017-09-04 09:57:10 +0800

    测试

---
+++ xmltest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@version: Python3.6
+@author: yanlong.liang
+@software: PyCharm
+@file: xmltest.py
+@time: 2017/8/30 下午1:24
+
+The very beginning mind itself is 
+    the most accomplished mind of true enlightenment.
+"""
+
+
