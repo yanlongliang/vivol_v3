aa1e8922973be9df4342f091ac42f296352088e4
yanlongliang <573477106@qq.com>
2018-07-23 10:58:25 +0800

    练习Django

---
+++ Djangotest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@version: Python3.6
+@author: yanlong.liang
+@software: PyCharm
+@file: Djangotest.py.py
+@time: 2018/7/23 上午10:50
+
+The very beginning mind itself is 
+    the most accomplished mind of true enlightenment.
+"""
+
+
+
